e433590f1c73cb2535f25e0c83f3342d2f8c036b
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-03-04 13:51:28 +0000

    add MediaEntity class and update pom.xml with MapStruct and validation dependencies

---
+++ video-service/src/main/java/com/videoservice/service/MediaEntity.java
@@ -0,0 +1,28 @@
+package com.videoservice.service;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.*;
+
+@Document(collection = "media")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MediaEntity {
+
+    @Id
+    private String id; // MongoDB IDs are usually Strings (ObjectIds)
+
+    private String title;
+    private String description;
+    private String thumbnailUrl;
+    private String trailerUrl;
+    private Integer duration;
+    private Integer releaseYear;
+    private String type;
+    private String category;
+    private Double rating;
+    private String director;
+    private String cast;
+}
